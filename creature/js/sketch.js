7c0a3eb69ab2a63e5c3f6b2a59a1c14cf0181a01
Leon Eckert <leoneckert@mailbox.org>
2024-10-18 10:57:13 +0800

    Initial commit

---
+++ creature/js/sketch.js
@@ -0,0 +1,30 @@
+let xArray = []; // initialize empty array
+let yArray = [];
+let initialSizeOfArray = 100;
+
+function setup() {
+  let cnv = createCanvas(800, 500);
+  cnv.parent("p5-canvas-container")
+  
+  for(let i = 0; i < initialSizeOfArray; i++){
+    xArray[i] = random(0, width);
+    yArray[i] = random(0, height);
+  }
+  
+}
+
+function draw() {
+  background(220);
+  
+  for(let i = 0; i < xArray.length; i++){
+    xArray[i]+=random(-1,1);
+    yArray[i]+=random(-1,1);
+    
+    let x = xArray[i];
+    let y = yArray[i];
+    circle(x, y, 20);
+  }
+  
+  // xArray[0]++
+  
+}
